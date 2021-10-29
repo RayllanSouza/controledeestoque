2e6026e5988e3a9124756b4040c4374be8e9f9bb
rayllan7029 <rayllan51@gmail.com>
2021-10-28 23:57:01 -0300

    axios dependency add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 import logo from './svgs/login.svg';
 
 function App() {
+  const [logged, setLogged] = useState(0);
   function formSubmit(event){
     event.preventDefault();
+    const login = event.target[0].value;
+    const password = event.target[1].value;
+    axios.post('http://localhost:3333/login', {
+      "login": login,
+      "senha": password
+    })
+    .then(function (response) {
+      setLogged(1)
+    })
+    .catch(function (error) {
+      setLogged(0)
+    });
   }
   return (
     <div className="bg">
-      <div className="login-content">
-        <img src={logo} alt="login svg" className="img-login"/>
-        <h1>Login</h1>
-        <form onSubmit={formSubmit}>
-          {/* <label>Usuario</label> */}
-          <input className="input-login" placeholder="Usuario" name="user"/>
-          {/* <label>Senha</label> */}
-          <input className="input-login" placeholder="Senha" name="password"/>
-          <button type="submit" className="button-login">Entrar</button>
-        </form>
-      </div>
+      {logged === 0 ? (
+            <div className="login-content">
+            <img src={logo} alt="login svg" className="img-login"/>
+            <h1>Login</h1>
+            <form onSubmit={formSubmit}>
+              <input className="input-login" placeholder="Usuario" name="user"/>
+              <input className="input-login" placeholder="Senha" type="password" name="password"/>
+              <button type="submit" className="button-login">Entrar</button>
+            </form>
+          </div>
+      ): logged === 1 ?(<div className="login-content">
+        <h1>Logado</h1>
+    </div>):null}
     </div>
   );
 }
